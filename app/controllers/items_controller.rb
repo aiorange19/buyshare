a69f46d8dd5023d631c19da686a76e1695f47c15
aiorange19 <aiorange19@gmail.com>
2019-04-12 21:30:31 +0900

    WIP item_edit
    ・投稿編集画面途中

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -36,14 +36,33 @@ class ItemsController < ApplicationController
   end
     
   def edit
+    @item = Item.find_by(id: params[:id])
+    @images = Image.find_by(id: params[:id])
   end
     
   def update
+    @item = Item.find_by(id: params[:id])
+      
+    if @item.update(content: params[:item][:content], buy_place: params[:item][:buy_place], price: params[:item][:price])
+        flash[:notice] = '投稿を編集しました'
+        redirect_to(item_path(@item.id))
+    else
+        render('users/edit.html.erb')
+    end  
   end
     
   def destroy
   end
   
+   def ensure_correct_user
+    @item = Item.find_by(id: params[:id])
+    
+    if @item.user_id != @current_user.id
+      flash[:notice] = "権限がありません"
+      redirect_to(items_path)
+    end
+  end
+    
  private
     
   def item_params
