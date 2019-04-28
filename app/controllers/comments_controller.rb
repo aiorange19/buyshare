14420fc1c03c0e2f874e73d3e486f52e142f526a
aiorange19 <aiorange19@gmail.com>
2019-04-29 00:16:26 +0900

    create comment_function_1
    ・投稿にコメントできるようにした
    ・コメントしたユーザーであれば、コメントを削除できるようにした

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,24 @@
 class CommentsController < ApplicationController
+  def create
+    @comment = Comment.new(comment_params)
+    @comment.user_id = @current_user.id
+    @comment.item_id = params[:item_id]
+      
+    if @comment.save
+      redirect_to("/items/#{params[:item_id]}")
+    else
+      render template: "items/show"
+    end
+
+  end
+    
+  def destroy
+    @comment = Comment.find(params[:id]).destroy
+    redirect_to("/items/#{params[:item_id]}")
+  end
+
+  private
+  def comment_params
+    params.require(:comment).permit(:content)
+  end
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,2 +1,6 @@
 class Comment < ApplicationRecord
+  validates :content, {presence: true, length: {minimum: 1, maximum: 150}}
+    
+  belongs_to :user
+  belongs_to :item
 end
