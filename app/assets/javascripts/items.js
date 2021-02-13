4c5fdda54cdc2a857d55743b4f2d79504e66f117
ai sagawa <>
2021-02-14 02:35:51 +0900

    JSとstylesheetを読み込めるように変更

---
+++ app/assets/javascripts/items.js
@@ -0,0 +1,16 @@
+//  Place all the behaviors and hooks related to the matching controller here.
+//  All this logic will automatically be available in application.js.
+//  You can use CoffeeScript in this file: http://coffeescript.org/
+// $(document).on,function() {
+//   $('#item-tags').tagit
+//     var fieldName = 'category_list'
+//     var singleField = true
+//   $('#item-tags').tagit()
+//   var category_string = $("#category_hidden").val()
+//   try
+//     var category_list = category_string.split(',')
+//     for tag in category_list
+//       $('#item-tags').tagit 'createTag', tag
+//   catch error
+// }
+  
